d19623d5befc87b672a0eb1d7a65bc07701f531b
karenjli <karenjy.li@gmail.com>
2019-08-23 11:16:28 -0500

    character page completed

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,19 @@
 import React from "react";
+import {Card, Image} from 'semantic-ui-react'
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return (
+        <div className="CharCard">
+          <Card>
+            <Image src={props.image} />
+              <Card.Header>{props.name}</Card.Header>
+              <Card.Meta>{`${props.species} ${props.status}`}</Card.Meta>           
+              <Card.Description>
+                Location: {props.location} 
+                {'\n'}
+                Origin: {props.origin}
+              </Card.Description>
+          </Card>
+        </div>
+    );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,41 @@
 import React, { useEffect, useState } from "react";
+import CharacterCard from './CharacterCard'
+import axios from "axios";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState ([]);
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+      
+      const getChar = () => {
+      axios
+        .get ("https://rickandmortyapi.com/api/character/")
+          .then (response => {
+            console.log(response.data.results);         
+            setCharacters (response.data.results) 
+          })
+          .catch (error => {
+            console.log(error);
+          })
+       }
+
+       getChar();
   }, []);
 
   return (
     <section className="character-list grid-view">
-      <h2>TODO: `array.map()` over your state here!</h2>
+       <div>
+         {characters.map (char =>
+            (<CharacterCard 
+              image={char.image} 
+              name={char.name} 
+              species={char.species} 
+              status={char.status} 
+              location={char.location.name}
+              origin={char.origin.name}/>
+            ))}  
+      </div>
     </section>
   );
 }
